4226071daaea4bef374c73274a03da61042d5f66
Victoria-Lopez-dev <72264965+Victoria-Lopez-dev@users.noreply.github.com>
2022-10-26 12:30:32 -0300

    codigo en clase 10

---
+++ Clase10/script.js
@@ -0,0 +1,48 @@
+//Comentarios en una sola linea
+/* comentarios 
+    en 
+    varias
+    lineas */
+
+//todo el codigo de JavaScript
+
+//console.log()// herramienta de JavaScript que nos permite ver por consola lo que le coloquemos dentro de los parentesis -> herramienta del Desarrollador.
+
+//los datos en tipos
+
+//STRINGS -> caracteres alfanumericos y se escriben entre comillas
+
+ "Juan" 
+// 'Admin123' 
+// `LKJLKJASD sjdhskhfsdjfh fjshfjkhdjkfhswueyrhjkdcohewnfc .`
+// "true" //string
+// '4512'
+// `undefined` //string
+// //NUMBERS o NUMEROS -> numeros se escriben sin ningun simbolo extra o detalle. Si es un numero decimal le coloco un punto (.) no coma (,)
+
+// 4512
+// //1,5 no 
+// 1.5 //si
+
+
+// //BOOLEANS o BOOLEANOS -> Verdadero y Falso palabras recervadas
+
+// true //es verdadero 
+// false //es falso 
+
+// //UNDEFINED o INDEFINIDO -> valor que inidica que lo obtenido no esta definido dentro de los datos.
+
+// undefined
+
+// //NULL o NULO -> vacio no tiene contenido
+
+// null
+
+//typeof(); -> nos permite ver que tipo de datos tenemos entre los parentesis
+
+typeof(4512) // lo hace pero como no le pedi que lo muestre no lo veo
+console.log(typeof("Juan"));
+console.log(45454);
+console.log(true);
+console.log("juan",456456,false)
+console.log("hola","don","pepito")
---
+++ Clase10/variables.js
@@ -0,0 +1,14 @@
+//variable nos va a permitir guardar informacion en un espacio de memoria y podamos utilizarlo cada vez que lo necesitemos.
+
+//una palabra recervada el nombre de la variable y su valor
+
+//definiendo o creando una variable
+var nombreAlumnx = "Juan Perez";
+
+nombreAlumnx
+
+
+console.log(typeof(nombreAlumnx))
+
+
+console.log(nombreAlumnx);
