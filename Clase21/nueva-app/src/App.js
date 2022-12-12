529b5358009abe5089c458b0bdf84c1afcbb3389
Victoria-Lopez-dev <72264965+Victoria-Lopez-dev@users.noreply.github.com>
2022-12-12 10:33:24 -0300

    clase 21- codigo en clase

--- Clase21/nueva-app/src/App.js
+++ Clase21/nueva-app/src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
   return (
     <div className="App">
      <h1>Hola Soy un titulo</h1>
+      <button className="btn btn-success">Ana</button>
     </div>
   );
 }
