42144a4e4b454d1989c43bb9328a2738b3b913ed
Victoria-Lopez-dev <72264965+Victoria-Lopez-dev@users.noreply.github.com>
2022-12-19 11:47:09 -0300

    clase 23- seccion Characters

--- proyecto/proyecto-app/src/components/Characters/Characters.js
+++ proyecto/proyecto-app/src/components/Characters/Characters.js
@@ -5,7 +5,31 @@ import{useEffect, useState} from 'react';
 
 export default function Characters() {
     let [datos,setDatos]=useState([]);
+    let [infoCompleta,setInfoCompleta]=useState([]);
  
+    const mostrarValor =(event)=>{
+
+        // se fija si esta o no checked,tildado
+        if(event.target.checked){
+                let datoFiltro=event.target.value;
+                if(datoFiltro === "Alive" || datoFiltro === "Dead"){
+                let resultado= datos.filter((ch)=> ch.status === datoFiltro)//nuevo array con solo los elementos que cumplen la condicion
+                    setDatos(resultado)
+                }
+                if(datoFiltro === "Female" || datoFiltro === "Male"){
+                    let resultado= datos.filter((ch)=> ch.gender === datoFiltro)
+                    setDatos(resultado)
+                }
+                if(datoFiltro === "unknown"){
+                    let resultado= datos.filter((ch)=> ch.origin.name === datoFiltro)
+                    setDatos(resultado)
+                } 
+                
+         }else{
+            setDatos(infoCompleta)
+         }
+    }
+
     const traerInfo=async()=>{
  
         let info= await fetch("https://rickandmortyapi.com/api/character")
@@ -15,23 +39,31 @@ export default function Characters() {
         
     }
 
-
+      //saco la funcion por fuera para que lo pueda utilizar en mi funcion del evento
+    const mostarInfo =async ()=>{
+        let dato= await traerInfo()
+        let infoPersonajes= dato.results
+        console.log(infoPersonajes)
+        setDatos(infoPersonajes)
+        setInfoCompleta(infoPersonajes)
+    }
+    //Se ejecuta cuando se renderiza la pagina
      useEffect(()=>{
-
-        const mostarInfo =async ()=>{
-            let dato= await traerInfo()
-            let infoPersonajes= dato.results
-            setDatos(infoPersonajes)
-            
-        }
-        mostarInfo()
+        mostarInfo() 
      },[])
 
 
     return(
         <div>
             <Navegation />
-            <Filters />
+            <section className='bg-success d-flex p-2'>
+                <Filters nombreFiltro="Character Alive" datoFiltro="Alive" muestraValor={mostrarValor}/> 
+                <Filters nombreFiltro="Character Dead" datoFiltro="Dead" muestraValor={mostrarValor}/> 
+                <Filters nombreFiltro="Female" datoFiltro="Female" muestraValor={mostrarValor}/> 
+                <Filters nombreFiltro="Male" datoFiltro="Male" muestraValor={mostrarValor}/> 
+                <Filters nombreFiltro="Origin Unknown" datoFiltro="unknown" muestraValor={mostrarValor}/> 
+            </section>
+                          
             <SectionCards datos={datos} />
         </div>
     )
--- proyecto/proyecto-app/src/components/FIlters/Filters.js
+++ proyecto/proyecto-app/src/components/FIlters/Filters.js
@@ -1,5 +1,10 @@
-export default function Filters() {
+export default function Filters({nombreFiltro,datoFiltro,muestraValor}) {
+
     return(
-        <h2>Seccion Filtros!</h2>
+        /* cambiamos class por className y for por htmlFor para que diferencientre el bucle y el atributo o el nombre de una clase con clases POO*/
+        <div className="form-check form-switch mx-2 bg-warning">
+            <input className="form-check-input" type="checkbox" role="switch" id={datoFiltro} value={datoFiltro} onChange={muestraValor}/>
+            <label className="form-check-label" htmlFor={nombreFiltro}>{nombreFiltro}</label>
+        </div>
     )
 }
